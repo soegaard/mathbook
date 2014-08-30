2010f612a8d2496f91db7c73e040153f12ef7379
Jens Axel Søgaard <jensaxel@soegaard.net>
2014-08-30 21:08:25 +0200

    Add internationalization support

--- mathbook/mathbook.rkt
+++ mathbook/mathbook.rkt
@@ -1,6 +1,44 @@
 #lang racket/base
-(require scribble/manual)
+(require racket/format
+         racket/match
+         scribble/core
+         scribble/decode  ; decode-content
+         scribble/manual)
 
-(provide title 
+(provide author
+         proof
          section
-         tt)
+         subsection
+         theorem
+         title
+         tt
+         current-language
+         danish-language 
+         english-language)
+
+(define english-language #hash())
+(define danish-language
+  #hash( ("Theorem" . "Sætning")
+         ("Proof"   . "Bevis")))
+
+(define current-language (make-parameter #f)) ; #f = english
+; (define current-language (make-parameter danish))
+
+(define (lookup s)
+  (match (current-language)
+    [#f s]
+    [_  (hash-ref  s (λ() s))]))
+    
+
+(define (theorem s . strs)
+  (let ([ss strs])
+    (paragraph plain
+               (cons (make-element 'bold (~a (lookup "Theorem") " "))
+                     (cons (make-element 'italic s)
+                           (decode-content ss))))))
+
+(define (proof . s)
+  (make-paragraph plain
+                  (cons (make-element 'bold (~a (lookup "Proof") " "))
+                        (decode-content s))))
+
